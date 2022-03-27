6bf99bdca9023dc757248cbc1939faa51663c481
fedefabre <federico.fabre@xpo.com>
2022-03-27 18:42:39 -0300

    Debounce on filtering added

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { applyingFilter } from './city/store/action/city.actions';
 import { CityState } from './city/store/reducer/city.reducer';
+import { debounce, debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,23 @@ import { CityState } from './city/store/reducer/city.reducer';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  filter = '';
+export class AppComponent implements OnInit {
+  private subject: Subject<string> = new Subject();
 
   constructor(private store: Store<CityState>) { }
 
-  filtering(event: any) {
-    console.log('dispatching', event.target.value)
-    this.store.dispatch(applyingFilter(event.target.value));
+  ngOnInit() {
+    // Wait at least half a second until the last key pressed avoiding dispatching several searches
+    this.subject.pipe(debounceTime(500)).subscribe(searchTextValue => {
+      this.store.dispatch(applyingFilter(searchTextValue));
+    });
+  }
+
+  inputKeyPressed(event: any) {
+    const searchedText = event.target.value;
+    // Search with 3 letters or when input is empty to return all list
+    if (searchedText.length === 0 || searchedText.length > 2) {
+      this.subject.next(searchedText);
+    }
   }
 }
