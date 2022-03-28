663c3df14e5fe34a8920283cf7fa025d83e03f0e
fedefabre <federico.fabre@xpo.com>
2022-03-28 02:16:53 -0300

    Fixing bolding pipe

--- app/src/app/city/pipes/bolding.pipe.ts
+++ app/src/app/city/pipes/bolding.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class BoldingPipe implements PipeTransform {
 
   transform(value: string | undefined, text: string): string {
-    return value ? value.replace(text , '<strong>' + text  + '</strong>') : '';  
+    return value ? value.replace(new RegExp(text, 'gi'), "<strong>$&</strong>") : '';
   }
-
 }
