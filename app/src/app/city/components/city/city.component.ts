9df6dd5df63693229095031260fbbd3c5f969ddc
fedefabre <federico.fabre@xpo.com>
2022-03-27 21:39:07 -0300

    Loader when preferring

--- app/src/app/city/components/city/city.component.ts
+++ app/src/app/city/components/city/city.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { City } from '../../../models/city';
 import { CitiesService } from '../../../cities.service';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-city',
@@ -11,12 +12,28 @@ import { CitiesService } from '../../../cities.service';
 export class CityComponent {
   @Input() city: City;
   @Input() filterText: string = '';
+  loading = false;
+  favorite = false;
 
   constructor(private service: CitiesService) { }
 
-  toggleSelection(event: MatCheckboxChange): void {
-    this.service.patchPreferred(this.city.geonameid, event.checked).subscribe(a => {
-      console.log(a);
-    })
+  toggleSelection(event: MatCheckboxChange, retry: number = 0): void {
+    this.loading = true;
+    // Retry 3 times until emit error to parent
+    if (retry === 3) {
+      throwError(()=>'An unexpected error ocurred. Please retry')
+    }
+    this.favorite = event.checked;
+    this.service.patchPreferred(this.city.geonameid, event.checked)
+      .pipe(
+        catchError(err => {
+          // If fail, retry
+          this.toggleSelection(event, retry++);
+          return throwError(() => err);
+        })
+      )
+      .subscribe( a => {
+        this.loading = false;
+      })
   }
 }
